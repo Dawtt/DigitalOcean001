0b0d1d2b762a4fa19ec3148b971037421c363a50
dw <davidwhitebird@gmail.com>
2019-02-20 07:41:17 -0600

    Before config script consolidation

--- configpi.sh
+++ configpi.sh
@@ -5,4 +5,28 @@
 REMOTE_USERNAME="-P 23835 root"
 REMOTE_HOST="68.168.165.164"
 LOCAL_ROOT="/Users/dw/Dropbox/GitHub/DigitalOcean001/pi"
-INTRO="THIS WILL COPY FILES FROM THE PI"
+INTRO_GET="THIS WILL COPY FILES FROM THE PI"
+INTRO_POST="POSTING TO PI!!!!!!!!!!!!!!!!!!!"
+
+###     FILES TO BE COPIED ARRAY
+FILES_LIST=("/etc/apache2/apache2.conf"
+"/etc/apache2/sites-available/000-default.conf"
+"/etc/apache2/sites-enabled/000-default.conf"
+"/etc/libapache2-mod-jk/workers.properties"
+"/etc/apache2/mods-available/jk.conf"
+"/var/lib/tomcat8/conf/server.xml"
+"/var/lib/tomcat8/conf/catalina.properties"
+"/var/lib/tomcat8/conf/tomcat-users.xml"
+"/var/lib/tomcat8/conf/web.xml")
+###     DIRECTORIES REQUIRED FOR FILE CREATION ARRAY
+DIRECTORIES=("/etc/apache2/"
+"/etc/apache2/sites-available/"
+"/etc/apache2/sites-enabled/"
+"/etc/libapache2-mod-jk/"
+"/etc/apache2/mods-available"
+"/var/lib/tomcat8/conf/")
+
+###     DIRECTORIES FOR RECURSIVE COPY OPTION ARRAY
+DIRECTORIES_RECURSIVE_COPY_LIST=("/etc/apache2/"
+"/etc/libapache2-mod-jk/"
+"/var/lib/tomcat8/conf/")
--- getConfigFiles.sh
+++ getConfigFiles.sh
@@ -3,6 +3,36 @@
 printf '\e[8;25;130t' #set terminal size
 clear
 
+
+interactive=
+filename=~/sysinfo_page.html
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                filename=$1
+                                ;;
+        -i | --interactive )    interactive=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+        -pb
+
+    esac
+    shift
+done
+
+
+
+
+
+
+
+
+
 #########     VARIABLES & PATHS         ############
 
 ###     COPY TYPE
@@ -39,10 +69,10 @@ printf "########################################################################
 printf "\n\n\ Press p for pi, b for belly; followed by <ENTER>.\n\n"
 read usrconf
 if [[ "$usrconf" = "p" ]]; then
-    source configpi.sh
+    source ${LOCAL_ROOT}/configpi.sh
 fi
 if [[ "$usrconf" = "b" ]]; then
-    source configdo.sh
+    source ${LOCAL_ROOT}/configdo.sh
 fi
 #########       END VARIABLES & PATHS         ############
 
@@ -51,7 +81,7 @@ fi
 ###     BEGIN SCRIPT
 
 printf "\n\n\n###########################################################################"
-printf "\n      %10s ${INTRO} \n" # $Intro is from config file source
+printf "\n      %10s ${INTRO_GET} \n" # $Intro is from config file source
 printf "###########################################################################"
 printf "\n\n"
 printf " %10s Script Settings:\n"
